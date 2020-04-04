66bf244612a2037e0b5d5658cc52270ee1394dce
Hanae05 <chacopipi0516@icloud.com>
2020-04-04 22:11:02 +0900

    index.jsの作成

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,71 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+var knex = require('knex')({
+  dialect: 'mysql',
+  connection: {
+    host    : 'localhost',
+    user    : 'root',
+    password: '',
+    database: 'my-nodeapp-db',
+    charset : 'utf-8'
+  }
+});
+
+var Bookshelf = requrie('bookshelf')(knex);
+
+Bookshelf.plugin('pagination');
+
+var User = Bookshelf.Model.extend({
+  tableName: 'users'
+});
+
+var Markdata = Bookshelf.model.extend({
+  tableName    : 'Markdata',
+  hasTimestamps: true,
+  user         : function() {
+    return this.belongsTo(User);
+  }
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.session == null) {
+    res.redirect('/login');
+    return;
+  }
+  new Markdata(['title']).orderBy('created_at', 'DESC')
+      .where('user_id', "=", req.session.login.id)
+      .fetchPage({page: 1, pageSize: 10, widhRelated: ['user']})
+      .then((collection) => {
+        var data = {
+          title: 'Markdown Search',
+          login: req.session.login,
+          message: '※最近のデータ',
+          form: {find: ''},
+          content: collection.toArray()
+        };
+        res.render('index', data);
+      })
 });
+
+router.post('/', function(req, res, next) {
+  new Markdata().orderBy('created_at', 'DESC')
+      .where('content', 'like', '%' + req.body.find + '%')
+      .fetchAll({widhRelated: ['user']})
+      .then((collection) => {
+      var data = {
+        title: 'Markdown Search',
+        login: req.session.login,
+        message: '※"' + req.body.find + '" で検索された最近の投稿データ',
+        form: req.body,
+        conent: collection.toArray() 
+      }
+      res.render('index', data);
+    })
+})
 
 module.exports = router;
