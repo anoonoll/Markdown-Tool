cf0ac589356dfebc75e8fa933b74164f971c7e87
Hanae05 <chacopipi0516@icloud.com>
2020-04-05 18:56:52 +0900

    完成

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('mysql');
+
 const { check, validationResult } = require('express-validator');
 
 var knex = require('knex')({
     client: 'mysql',
     connection: {
-        host    : 'localhost',
+        host    : '127.0.0.1',
         user    : 'root',
         password: '',
         database: 'my-nodeapp-db',
@@ -24,8 +25,8 @@ var User = Bookshelf.Model.extend({
 router.get('/', (req, res, next) => {
     var data = {
         title: 'Login',
-        content: {name: '', password: ''},
-        form: '名前とパスワードを入力下さい。'
+        form: {name: '', password: ''},
+        content: '名前とパスワードを入力下さい。'
     }
     res.render('login', data);
 })
@@ -33,7 +34,7 @@ router.get('/', (req, res, next) => {
 router.post('/', 
 
 [
-    check('username', 'NAMEは必ず入力して下さい。').notEmpty(),
+    check('name', 'NAMEは必ず入力して下さい。').notEmpty(),
     check('password', 'PASSWORDは必ず入力して下さい。').notEmpty()
 ],
 
@@ -41,47 +42,54 @@ router.post('/',
     var request = req;
     var response = res;
 
+    const errors = validationResult(req);
     
-    req.getValidationResult().then((result) => {
-        if (!result.isEmpty()) {
-            var content = '<ul class="error">';
-            var result_arr = result.array();
-            for(var n in result_arr) {
-                content += '<li>' + reault_arr[n].msg + '</li>'
-            }
-            content += '</ul>';
-            var data = {
-                title: 'Login',
-                content: content,
-                form: req.body
-            }
-            response.render('login', data);
-        } else {
-            var nm = req.body.name;
-            var pw = req.body.password;
-
-            User.query({where: {name: nm}, andWhre: {password: pw}})
-              .fetch()
-              .then((model) => {
-                  if (model == null) {
-                      var data = {
-                          title: '再入力',
-                          content: '<p class="error">名前またはパスワードが違います</p>',
-                          form: req.body
-                      };
-                      respose.render('login', data);
-                  } else {
-                      request.session.login = model.attributes;
-                      var data = {
-                          title: 'Login',
-                          content: '<p>ログインしました!<br>トップページに戻ってメッセージを送信下さい。</p>',
-                          form: req.body
-                      }
-                      respose.render('login', data);
-                  }
-              });
+    if (!errors.isEmpty()) {
+        var content = '<ul class="error">';
+        var result_arr = errors.array();
+        for(var n in result_arr) {
+            content += '<li>' + result_arr[n].msg + '</li>'
         }
-    })
+        content += '</ul>';
+        var data = {
+            title: 'Login',
+            content: content,
+            form: req.body
+        }
+        response.render('login', data);
+    } else {
+        var nm = req.body.name;
+        var pw = req.body.password;
+
+        User.query({where: {name: nm}, andWhere: {password: pw}})
+            .fetch()
+            .then((model) => {
+                if (model == null) {
+                    var data = {
+                        title: '再入力',
+                        content: '<p class="error">名前またはパスワードが違います</p>',
+                        form: req.body
+                    };
+                    response.render('login', data);
+                } else {
+                    request.session.login = model.attributes;
+                    var data = {
+                        title: 'Login',
+                        content: '<p>ログインしました!<br>トップページに戻ってメッセージを送信下さい。</p>',
+                        form: req.body
+                    }
+                    response.render('login', data);
+                }
+            }).catch((error) => {
+                var data = {
+                  title: '再入力',
+                  content: '<p class="error">名前またはパスワードが違います。</p>',
+                  form: req.body
+                };
+                res.render('login', data);
+                console.log(error);
+            });
+    }
 });
 
 module.exports = router;
